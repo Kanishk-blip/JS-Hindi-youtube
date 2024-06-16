32ed35126f80402efb93364c65e0809f375b33af
Kanishk-blip <agarwalkanishk9@gmail.com>
2024-06-16 02:56:16 +0000

    datatype

---
+++ 01_basics/01datatype.js
@@ -0,0 +1,19 @@
+"use strict"; // treats all JS code according to new version
+//alert(3+3) // we are using node js not browser
+
+console.log(3+
+    3
+)              // code readability should be high
+ 
+let name="Kanishk";
+let age=19;
+let isLoggedIn=true;
+ 
+
+// number => 2^53
+//bigint
+// String => ""
+//boolean => true/false
+// null => stand alone value
+//undefined 
+//symbol 
---
+++ 01_basics/01variable.js
@@ -0,0 +1,11 @@
+console.log("kanishk");
+accountName=123;
+console.log(accountName);
+const accountPassword="egkf";
+let accountId="k!123";
+console.table([accountName,accountPassword,accountId]);
+let password="123";
+console.log(password);
+var dash = "ke";
+console.log(dash);
+// do not use var to declare variable
